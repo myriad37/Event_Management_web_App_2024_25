be1eda2e4a29a93f76d5160034aa49f09700f8b1
sabahabtamu <sabahabtamuu@gmail.com>
2025-01-09 13:05:43 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,14 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './typeorm/entities/User';
 import { Role } from './typeorm/entities/Role';
-import { UsersModule } from './users/users.module';
-import { EventModule } from './event/event.module';
-import { RoleModule } from './role/role.module';
-import { EventsController } from './events/controllers/events/events.controller';
-import { EventsService } from './events/services/events/events.service';
-import { EventModule } from './event/event.module';
 
 @Module({
   imports: [AuthModule, TypeOrmModule.forRoot({
     type: 'mysql',
     host: 'localhost',
     port: 3306,
-    username: 'root',
-    password: 'pa55word@Play04',
+    username: '',
+    password: '',
     database: 'event_management_system',
     entities: [User, Role, Event],
     synchronize: true,
