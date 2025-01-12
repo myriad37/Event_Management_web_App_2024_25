7c3520229c34acc42d2d375f1ed7b437f1daf971
blen gebre <blengebre2121@gmail.com>
2025-01-12 00:08:56 -0800

    backend

---
+++ taatee-backend/jest.config.js
@@ -0,0 +1,10 @@
+// jest.config.js
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/*.spec.ts'],
+    transform: {
+      '^.+\\.ts$': 'ts-jest',
+    },
+  };
+  
